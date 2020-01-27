fac6f0975da49275e793592b14df3375fef33d9d
Zaid Qureshi <zaidqureshi92@gmail.com>
2020-01-26 18:24:28 -0600

    plots

--- Plot3.R
+++ Plot3.R
@@ -6,18 +6,11 @@ df3$Date <-dmy(df3$Date)
 df3$Time <- hms(df3$Time, roll = FALSE)
 df3$x <- with(df3, Date +Time)
 
-png("plot4.png", width=480, height=480)
-par(mfcol =c(2,2))
 
-with(df3, plot(x, Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)"))
-
+png("plot3.png", width=480, height=480)
 with(df3, plot(x, Sub_metering_1, type = "n"))
 with(df3,lines( x, Sub_metering_1, col = "Black"))
 with(df3,lines( x, Sub_metering_2, col = "red"))
 with(df3,lines( x, Sub_metering_3, col = "blue"))
-legend("topright",lty = 1, lwd = 2, col = c("Black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),cex = 0.5)
-
-
-with(df3, plot(x, Voltage, type="l", xlab="datetime"))
-with(df3, plot(x, Global_reactive_power , type="l", xlab="datetime"))
+legend("topright",lty = 1, lwd = 2, col = c("Black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 dev.off()
